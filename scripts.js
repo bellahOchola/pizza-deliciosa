213c0ca771be8bc057959074ac61de51f1022c7c
bellahOchola <bellahkenya@gmail.com>
2019-10-03 11:13:15 +0300

    add image at landing page

---
+++ scripts.js
@@ -0,0 +1,14 @@
+$("#pname"). click(function(){
+  var mine=  $('#pname').val();
+    alert(mine);
+});
+
+$("#psize"). click(function(){
+    var size=  $('#psize option:selected').val();
+      alert(size);
+  });
+  
+  $("#pcrust"). click(function(){
+    var crust=  $('#pcrust').val();
+      alert(crust);
+  });
