8e88fd1431577c0e202162240e2341e808d4c603
bellahOchola <bellahkenya@gmail.com>
2019-10-04 10:32:51 +0300

    add constructor function

--- scripts.js
+++ scripts.js
@@ -12,3 +12,10 @@ $("#psize"). click(function(){
     var crust=  $('#pcrust').val();
       alert(crust);
   });
+
+  function Pizza(name, size, topping, crust){
+    this.name=name;
+    this.size=size;
+    this.topping=topping;
+    this.crust=crust;
+  }
