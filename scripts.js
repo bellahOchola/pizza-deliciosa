ce39a3ffcc0013e5b0424157fba4ee385f29839f
bellahOchola <bellahkenya@gmail.com>
2019-10-05 09:36:42 +0300

    create an instance of the constructor in js

--- scripts.js
+++ scripts.js
@@ -9,10 +9,16 @@ function Pizza(name, size, topping, crust){
 
 $("form").submit(function(){
   event.preventDefault();
-    prefer1 = $(".prefer").val();
+    prefer1 = $(".prefer").find(":selected").text();
     size1 = $("#siz").val();
     top1 =$("#top").val();
     crust1 =$("#crus").val();
-  });
 
+    var order= new Pizza(prefer1, size1, top1, crust1);
+    $("ul#myOrder").append("<li><span class='orders'>" + order.name + "</span></li>");
+
+    // $(".prefer").val("");
+    // $("input#new-last-name").val("");
+  });
+  
   
